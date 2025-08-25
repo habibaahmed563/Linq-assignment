c7a489095084aa45a42e6173ac6aebac578a51db
habibaahmedabdelmalek@outlook.sa <habibaahmedabdelmalek@outlook.sa>
2025-08-26 01:48:24 +0300

    Aggregate Operators

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.ComponentModel;
+using System.Globalization;
 using static Linq_assignment.ListGenerator;
 namespace Linq_assignment
 {
@@ -45,6 +47,101 @@ namespace Linq_assignment
 
             /* --------------------------------------------------------------*/
 
+            //LINQ - Aggregate Operators 
+
+            //1.
+
+            //int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var result =( from P in Arr
+            //             where P % 2 != 0
+            //             select P).Count();
+
+            //2.
+
+            //var result = (from P in Arr
+            //              select P).Sum();
+
+            //Console.WriteLine(result);
+
+            //3.
+
+            //String[] Words = File.ReadAllLines("dictionary_english.txt");
+            //var result = (from W in Words
+            //              select W.Length).Sum();
+            //Console.WriteLine(result);
+
+            //4.
+
+            //string[] Words = File.ReadAllLines("dictionary_english.txt");
+            //var result = (from W in Words
+            //              select W.Length).Min();
+            //Console.WriteLine(result);
+
+            //5.
+
+            //var result = from P in ProductList
+            //             group P by P.Category
+            //             into newcategory
+            //             select new
+            //             {
+            //                 Category = newcategory.Key,
+            //                 TotalUnits = newcategory.Sum(x => x.UnitsInStock)
+            //             };
+            //foreach(var item in result)
+            //{
+            //    Console.WriteLine($"{item.Category} , {item.TotalUnits}");
+            //}
+
+
+            //6.
+
+            //var result = from P in ProductList
+            //             group P by P.Category
+            //             into N
+            //             select new
+            //             {
+            //                 Category = N.Key,
+            //                 ChepestPrice = N.Min(x => x.UnitPrice)
+            //             };
+            //foreach(var item in result)
+            //{
+            //    Console.WriteLine($"{item.Category}, {item.ChepestPrice}");
+            //}
+
+            //7.
+
+            //var result = from P in ProductList
+            //             group P by P.Category into N
+            //             let MinPrice = N.Min(x => x.UnitPrice)
+            //             from P in N
+            //             where P.UnitPrice == MinPrice
+            //             select new
+            //             {
+            //                 Category = N.Key,
+            //                 Product = P.ProductName,
+            //                 Price=P.UnitPrice
+            //             };
+            //foreach(var item in result)
+            //{
+            //    Console.WriteLine($"{item.Category} , {item.Product} , {item.Price}");
+            //}
+
+            //8.
+
+            //var result = from P in ProductList
+            //             group P by P.Category
+            //             into N
+            //             select new
+            //             {
+            //                 Category = N.Key,
+            //                 Avgprice = N.Average(x => x.UnitPrice)
+            //             };
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine($"{item.Category} , {item.Avgprice}");
+            //}
+
+            /* --------------------------------------------------------------*/
 
 
         }
