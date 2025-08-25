0e69d8a985c2102290dbea2340bb6a34e6b036b1
habibaahmedabdelmalek@outlook.sa <habibaahmedabdelmalek@outlook.sa>
2025-08-26 00:18:51 +0300

    Restriction Operators

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Linq_assignment
+﻿
+using static Linq_assignment.ListGenerator;
+namespace Linq_assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //LiNQ - Restriction Operators 
+
+            //1. 
+            //var result = from P in ProductList
+            //             where P.UnitsInStock == 0
+            //             select P;
+
+            //2.
+            //var result = from P in ProductList
+            //             where P.UnitsInStock > 0 && P.UnitPrice > 3.00M
+            //             select P;
+
+            //foreach(var item in result )
+            //{
+            //    Console.WriteLine(item);
+            //}
+                         
+
         }
     }
 }
